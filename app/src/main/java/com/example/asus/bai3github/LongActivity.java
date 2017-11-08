d1aeae827b568510f180767802e7edec973e749a
khoavokeanh <khoavka.ot@gmail.com>
2017-11-08 22:37:59 +0700

    commit change longactivites

---
+++ app/src/main/java/com/example/asus/bai3github/LongActivity.java
@@ -0,0 +1,11 @@
+package com.example.asus.bai3github;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Asus on 11/8/2017.
+ */
+
+public class LongActivity extends AppCompatActivity {
+
+}
